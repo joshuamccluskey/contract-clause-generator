92ad953c1f6dfd55a88128cffcfba37ecac5f14a
joshuamccluskey <jpiff57@gmail.com>
2022-01-15 12:03:24 -0800

    first commit

---
+++ contract-clause-generator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace contract_clause_generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
